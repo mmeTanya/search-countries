2fd9e15d1839a6611bf258e719e5826eb562570a
Tetiana Legois <mme.tanyalegois@gmail.com>
2021-12-13 00:12:14 +0200

    1

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import debounce from 'lodash.debounce';
+import ApiServiseCountries from './js/fetchCountries';
+import './css/styles.css';
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchInput: document.querySelector('input#search-box'),
+  countryInfo: document.querySelector('.country-info'),
+  countryList: document.querySelector('.country-list'),
+};
+const apiServiseCountries = new ApiServiseCountries();
+
+refs.searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+  apiServiseCountries.value = event.target.value.trim();
+  apiServiseCountries.fetchCountries().then(countries => {
+    console.log(countries);
+    onCreateCountryCard(countries);
+  });
+}
+
+function onCreateCountryCard(countries) {
+  if (countries.length > 1 && countries.length < 10) {
+    const markupCountryList = countries
+      .map(country => {
+        return `<li>
+        <div class='overlay'>
+        <img src='${country.flags.svg}' alt='flag' width='30'/>
+        <h1 class='countries'>${country.name.official}</h1>
+        </div>
+        </li>`;
+      })
+      .join('');
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.insertAdjacentHTML('beforeend', markupCountryList);
+  } else if (countries.length === 1) {
+    const markupCountryInfo = countries.map(country => {
+      return `<div class='overlay'>
+      <img src='${country.flags.svg}' alt='flag' width='30'/>
+      <h1 class='country'>${country.name.official}</h1>
+      </div>
+      <p>Capital: ${country.capital}</p>
+      <p>Population: ${country.population}</p>
+      <p>Languages: ${Object.values(country.languages)}</p>`;
+    });
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo);
+  } else {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+  }
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default class ApiServiseCountries {
+  constructor() {
+    this.searchValue = '';
+  }
+  fetchCountries() {
+    const URL = `https://restcountries.com/v3.1/name/${this.searchValue}?fields=name,capital,population,flags,languages`;
+
+    return fetch(URL)
+      .then(response => {
+        if (!response.ok) {
+          Notify.failure('Oops, there is no country with that name');
+        }
+        return response.json();
+      })
+      .then(countries => {
+        if (countries.length > 10) {
+          Notify.info('Too many matches found. Please enter a more specific name');
+        }
+        return countries;
+      });
+  }
+
+  get value() {
+    return this.searchValue;
+  }
+
+  set value(country) {
+    this.searchValue = country;
+  }
+}
